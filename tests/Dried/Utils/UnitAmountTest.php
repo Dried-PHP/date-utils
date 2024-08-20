e8abe2d9416fb95f47ffa8fc0db466c321080b8b
Bastien Miclo <kylekatarnls@protonmail.com>
2024-08-20 23:03:38 +0200

    Test values for each unit or AmountUnit

--- tests/Dried/Utils/UnitAmountTest.php
+++ tests/Dried/Utils/UnitAmountTest.php
@@ -12,26 +12,32 @@ final class UnitAmountTest extends TestCase
     public static function getUnitAmounts(): array
     {
         return [
-            [UnitAmount::microseconds(12), Unit::Microsecond, 12.0],
-            [UnitAmount::milliseconds(1.5), Unit::Millisecond, 1.5],
-            [UnitAmount::seconds(999_999_999_999), Unit::Second, 999_999_999_999.0],
-            [UnitAmount::minutes(60), Unit::Minute, 60.0],
-            [UnitAmount::hours(-1), Unit::Hour, -1.0],
-            [UnitAmount::days(-365), Unit::Day, -365.0],
-            [UnitAmount::weeks(0), Unit::Week, 0.0],
-            [UnitAmount::months(13), Unit::Month, 13.0],
-            [UnitAmount::quarters(INF), Unit::Quarter, INF],
-            [UnitAmount::years(1), Unit::Year, 1.0],
-            [UnitAmount::decades(0.2), Unit::Decade, 0.2],
-            [UnitAmount::centuries(1_000), Unit::Century, 1_000.0],
-            [UnitAmount::millennia(M_PI), Unit::Millennium, M_PI],
+            [UnitAmount::microseconds(...), Unit::Microsecond],
+            [UnitAmount::milliseconds(...), Unit::Millisecond],
+            [UnitAmount::seconds(...), Unit::Second],
+            [UnitAmount::minutes(...), Unit::Minute],
+            [UnitAmount::hours(...), Unit::Hour],
+            [UnitAmount::days(...), Unit::Day],
+            [UnitAmount::weeks(...), Unit::Week],
+            [UnitAmount::months(...), Unit::Month],
+            [UnitAmount::quarters(...), Unit::Quarter],
+            [UnitAmount::years(...), Unit::Year],
+            [UnitAmount::decades(...), Unit::Decade],
+            [UnitAmount::centuries(...), Unit::Century],
+            [UnitAmount::millennia(...), Unit::Millennium],
         ];
     }
 
     #[DataProvider('getUnitAmounts')]
-    public function testName(UnitAmount $unitAmount, Unit $unit, float $amount): void
+    public function testName(callable $unitAmountCreator, Unit $unit): void
     {
-        self::assertSame($unit, $unitAmount->unit);
-        self::assertSame($amount, $unitAmount->amount);
+        $values = [12.0, 1.5, 999_999_999_999, 60, -1, -365, 0, 13, INF, 1, 0.2, 1_000, M_PI];
+
+        foreach ($values as $value) {
+            $unitAmount = $unitAmountCreator($value);
+            self::assertInstanceOf(UnitAmount::class, $unitAmount);
+            self::assertSame($unit, $unitAmount->unit);
+            self::assertSame((float) $value, $unitAmount->amount);
+        }
     }
 }
