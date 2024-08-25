609e80e2c6510c15d1c368ad6df587785665731d
Bastien Miclo <kylekatarnls@protonmail.com>
2024-08-25 15:32:27 +0200

    Implement unit range methods

--- src/Dried/Utils/Unit.php
+++ src/Dried/Utils/Unit.php
@@ -9,19 +9,19 @@ use InvalidArgumentException;
 
 enum Unit: string
 {
-    case Microsecond = 'microsecond';
-    case Millisecond = 'millisecond';
-    case Second = 'second';
-    case Minute = 'minute';
-    case Hour = 'hour';
-    case Day = 'day';
-    case Week = 'week';
-    case Month = 'month';
-    case Quarter = 'quarter';
-    case Year = 'year';
-    case Decade = 'decade';
-    case Century = 'century';
     case Millennium = 'millennium';
+    case Century = 'century';
+    case Decade = 'decade';
+    case Year = 'year';
+    case Quarter = 'quarter';
+    case Month = 'month';
+    case Week = 'week';
+    case Day = 'day';
+    case Hour = 'hour';
+    case Minute = 'minute';
+    case Second = 'second';
+    case Millisecond = 'millisecond';
+    case Microsecond = 'microsecond';
 
     public static function fromName(string $unit): self
     {
@@ -31,6 +31,67 @@ enum Unit: string
     public static function tryFromName(string $unit): ?self
     {
         return self::tryFrom(strtolower($unit));
+    }
+
+    /** @return list<self> */
+    public static function allBut(self ...$omittedUnits): array
+    {
+        return self::unitsWithout(self::cases(), $omittedUnits);
+    }
+
+    /**
+     * @param list<self> $skip
+     *
+     * @return list<self>
+     */
+    public static function between(Unit $from, Unit $to, array $skip): array
+    {
+        $units = [];
+        $fromMet = false;
+        $toMet = false;
+        $toMetFirst = false;
+
+        foreach (self::cases() as $unit) {
+            if ($unit === $from) {
+                $fromMet = true;
+                $units[] = $unit;
+
+                if ($toMet) {
+                    break;
+                }
+
+                continue;
+            }
+
+            if ($unit === $to) {
+                $toMet = true;
+                $units[] = $unit;
+
+                if ($fromMet) {
+                    break;
+                }
+
+                $toMetFirst = true;
+
+                continue;
+            }
+
+            if ($fromMet || $toMet) {
+                $units[] = $unit;
+            }
+        }
+
+        return self::unitsWithout($toMetFirst ? array_reverse($units) : $units, $skip);
+    }
+
+    /**
+     * @param list<self> $skip
+     *
+     * @return list<self>
+     */
+    public function to(Unit $to, array $skip): array
+    {
+        return self::between($this, $to, $skip);
     }
 
     public function modifier(int|float $value = 1): string
@@ -57,5 +118,20 @@ enum Unit: string
             self::Millennium => 'millennia',
             default => $this->value . 's',
         };
+    }
+
+    /**
+     * @param list<self> $units
+     * @param list<self> $omittedUnits
+     *
+     * @return list<self>
+     */
+    private static function unitsWithout(array $units, array $omittedUnits): array
+    {
+        return array_values(array_udiff(
+            $units,
+            $omittedUnits,
+            static fn (self $a, self $b) => $a->value <=> $b->value,
+        ));
     }
 }
